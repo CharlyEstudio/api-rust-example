2cc33760ddb6a7dde4d096d052ba426fcd531388
Charly Ramirez <pingestudio@gmail.com>
2023-06-24 15:43:21 -0600

    encriptar password

--- src/commands/users.rs
+++ src/commands/users.rs
@@ -1,3 +1,4 @@
+use argon2::{password_hash::{SaltString, rand_core::OsRng}, Argon2, PasswordHasher};
 use diesel::{PgConnection, Connection};
 
 use crate::{models::users::NewUser, repositories::{users::UserRepository, roles::RoleRepository}};
@@ -11,7 +12,12 @@ fn load_db_connection() -> PgConnection {
 
 pub fn create_user(username: String, password: String, roles_codes: Vec<String>) {
   let mut c = load_db_connection();
-  let new_user = NewUser {username, password};
+
+  let salt = SaltString::generate(OsRng);
+  let argon = Argon2::default();
+  let password_hash = argon.hash_password(password.as_bytes(), &salt).unwrap();
+
+  let new_user = NewUser {username, password: password_hash.to_string()};
   let user = UserRepository::create(&mut c, new_user, roles_codes).unwrap();
   println!("User created {:?}", user);
   let roles = RoleRepository::find_by_user(&mut c, &user).unwrap();
